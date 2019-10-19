135283e737114b41540867bc104cddd6485563c6
Wendy Harris <wendyv510@hotmail.com>
2019-10-19 19:17:15 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,10 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  your_hash = {
+         "name" => "Wendy"
+         "age" => "34"
+         "gender" => "female"
+  }
 
   
 end
